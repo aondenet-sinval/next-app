5f9e18ccbbac857d4f85a4cb6c2a52b9186974fd
aondenet-sinval <sinvalsatv@gmail.com>
2021-12-13 21:54:45 -0300

    começando a mexer no layout

---
+++ pages/pages/caroussel.js
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import Image from 'next/image'
+import Script from 'next/script'
+import React from 'react'
+import {
+	Container, Carousel, Button } from 'react-bootstrap/';
+
+
+
+export default function Caroussel() {
+	return <div>	 
+			      <Head>             
+			        <title>Caroussel</title>
+			      </Head>
+			      <div className="container">
+			      <h1>Caroussel</h1>
+			      <ul><li>
+			        <Link href="/">
+			          <a><Button className="badge">Voltar para home</Button></a>
+			        </Link>
+			      </li></ul>
+			      </div>
+					<Carousel>
+					  <Carousel.Item>
+					    <img
+					      className="d-block w-100"
+					      src="/images/img7.jpg"
+					      layout="fill"
+					      alt="First slide"
+					    />
+					    <Carousel.Caption>
+					      <h3>First slide label</h3>
+					      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					    </Carousel.Caption>
+					  </Carousel.Item>
+					  <Carousel.Item>
+					    <img
+					      className="d-block w-100"
+					      src="/images/img8.jpg"
+					      layout="fill"
+					      alt="Second slide"
+					    />
+					
+					    <Carousel.Caption>
+					      <h3>Second slide label</h3>
+					      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					    </Carousel.Caption>
+					  </Carousel.Item>
+					</Carousel>
+	</div>
+}
---
+++ pages/pages/example.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useState, React } from 'react'
+import {
+	Container,	Button, Offcanvas,
+	OffcanvasHeader, OffcanvasTitle,
+	OffcanvasBody } from 'react-bootstrap/';
+
+export default function ExampleCanvas() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+		return<>
+				   <Button variant="primary" onClick={handleShow}>
+			        Launch
+			      </Button>
+			
+			      <Offcanvas show={show} onHide={handleClose}>
+			        <Offcanvas.Header closeButton>
+			          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+			        </Offcanvas.Header>
+			        <Offcanvas.Body>
+							<h1 className="title">
+							  Ir para{' '}
+							  <Link href="/pages/contato">
+							    <a>Contato</a>
+							  </Link>
+							</h1>
+							<h1 className="title">
+							  Ir para{' '}
+							  <Link href="/pages/caroussel">
+							    <a>Carrossel</a>
+							  </Link>
+							</h1>
+			        </Offcanvas.Body>
+			      </Offcanvas>
+		</>
+}
+
+
